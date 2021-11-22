77f67b3098861b08da5b6bf603f3da75c69af3c5
Kaushik-cg-giri <78954349+Kaushik-cg-giri@users.noreply.github.com>
2021-11-22 14:13:22 +0530

    Update ProductRepository.java

--- smartshopApp/src/main/java/com/lti/smartshopApp/dao/ProductRepository.java
+++ smartshopApp/src/main/java/com/lti/smartshopApp/dao/ProductRepository.java
@@ -17,6 +17,6 @@ public interface ProductRepository extends JpaRepository<Products, Long> {
 	@Query(value="select p from Products p where p.category=?1")
 	Optional<Products> findbyCategory(String category);
 	
-	@Query(value="select p from Products where p.name=?1")
+	@Query(value="select p from Products p where p.name=?1")
 	 Optional<Products> findbyName(String name);
 }
